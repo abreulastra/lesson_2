234a53ea685ea77d6ba8aee1aa8257db530d7fa7
Raul Abreu <abreulastra@gmail.com>
2016-06-02 13:43:58 +0300

    Improve text

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson_2
+Says 'Hello, world". From the lesson 2, Thinkful - Data Science, about setting up Github.
--- lesson_2_H.py
+++ lesson_2_H.py
@@ -5,4 +5,4 @@ Created on Mon May 16 15:39:26 2016
 @author: AbreuLastra_Work
 """
 
-print ('Hello, World!')
+print ("Hello, World. I'm now becoming a git Ninga. ")
